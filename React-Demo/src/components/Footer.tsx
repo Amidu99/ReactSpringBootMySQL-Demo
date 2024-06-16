b34753b72bc38196c6dfddc8c34e70f60ea4c885
Amidu99 <amidushamika@gmail.com>
2024-06-16 20:28:02 +0530

    add Footer component

---
+++ React-Demo/src/components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <footer>
+            <div className="appFooter">
+                ©Copyright - All Right Reserved Designed by Amidu Shamika
+            </div>
+        </footer>
+    );
+}
+export default Footer;
