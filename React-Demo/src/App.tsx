8a105ebeb0485efe0184d657e0a695d453e59a7f
Amidu99 <amidushamika@gmail.com>
2024-06-16 20:29:34 +0530

    update App.tsx

--- React-Demo/src/App.tsx
+++ React-Demo/src/App.tsx
@@ -1,11 +1,29 @@
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './pages/Home';
+import Settings from './pages/Settings';
+import Users from './pages/Users';
+import UserProfile from './pages/UserProfile';
+import EmployeeProfile from './pages/EmployeeProfile';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Employees from "./pages/Employees";
 
-function App() {
-  return (
-    <>
-
-    </>
-  )
+export default function App() {
+  return(
+      <>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path='/' element={<Home />}></Route>
+              <Route path='/users' element={<Users />}></Route>
+              <Route path='/user/:userID' element={<UserProfile />}></Route>
+              <Route path='/employees' element={<Employees />}></Route>
+              <Route path='/employee/:employeeCode' element={<EmployeeProfile />}></Route>
+              <Route path='/settings' element={<Settings />}></Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </>
+  );
 }
-
-export default App;
