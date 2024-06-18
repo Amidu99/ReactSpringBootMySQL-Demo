86890d101f966a90f1aee04dabd0e428de49a9b0
Amidu99 <amidushamika@gmail.com>
2024-06-18 14:48:55 +0530

    update EmployeeService

--- SQL_BE/src/main/java/lk/ijse/sql_be/service/EmployeeService.java
+++ SQL_BE/src/main/java/lk/ijse/sql_be/service/EmployeeService.java
@@ -1,5 +1,14 @@
 package lk.ijse.sql_be.service;
 
-public interface EmployeeService {
+import lk.ijse.sql_be.dto.EmployeeDTO;
+import java.util.List;
 
+public interface EmployeeService {
+    boolean existsByEmployeeCode(String employeeCode);
+    void saveEmployee(EmployeeDTO employeeDTO);
+    EmployeeDTO getEmployeeByEmployeeCode(String employeeCode);
+    List<EmployeeDTO> getAllEmployees();
+    String getLastEmployeeCode();
+    void updateEmployee(EmployeeDTO employeeDTO);
+    void deleteEmployee(String employeeCode);
 }
