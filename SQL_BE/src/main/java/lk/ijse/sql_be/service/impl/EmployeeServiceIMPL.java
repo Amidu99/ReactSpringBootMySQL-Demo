f3b4c84f1f1defd4e0b2d066c4c05a617458f951
Amidu99 <amidushamika@gmail.com>
2024-06-18 14:34:54 +0530

    add EmployeeServiceIMPL

---
+++ SQL_BE/src/main/java/lk/ijse/sql_be/service/impl/EmployeeServiceIMPL.java
@@ -0,0 +1,13 @@
+package lk.ijse.sql_be.service.impl;
+
+import jakarta.transaction.Transactional;
+import lk.ijse.sql_be.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class EmployeeServiceIMPL implements EmployeeService {
+
+}
