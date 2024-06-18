a4106d9ecd24fe39529428c23163704200f4cc82
Amidu99 <amidushamika@gmail.com>
2024-06-18 14:49:40 +0530

    update EmployeeRepo

--- SQL_BE/src/main/java/lk/ijse/sql_be/repo/EmployeeRepo.java
+++ SQL_BE/src/main/java/lk/ijse/sql_be/repo/EmployeeRepo.java
@@ -2,7 +2,11 @@ package lk.ijse.sql_be.repo;
 
 import lk.ijse.sql_be.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface EmployeeRepo extends JpaRepository<Employee, String> {
-
+    boolean existsByEmployeeCode(String employeeCode);
+    Employee getEmployeeByEmployeeCode(String employeeCode);
+    @Query("SELECT MAX (e.employeeCode) FROM Employee e")
+    String getLastEmployeeCode();
 }
